e782c93667bc3511a0e8d532615831ebff4190ea
Tom Lynam <tom.s.lynam@gmail.com>
2014-08-07 00:04:11 -0400

    Add user image placeholder to listing page

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -72,7 +72,7 @@ class ListingsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def listing_params
-      params.require(:listing).permit(:name, :race, :raceday, :cause, :avatar)
+      params.require(:listing).permit(:name, :race, :raceday, :cause)
     end
 
     def check_user
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ActiveRecord::Base
 
   validates :name, presence: true
 
-  ##validates :avatar, presence: true
+  ##validates :avatar, presence: true 
 
   has_many :listings, dependent: :destroy
 
