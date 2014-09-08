939fdad59763b50a175754a79885c56ba5fb6108
Tom Lynam <tom.s.lynam@gmail.com>
2014-09-07 20:55:58 -0400

    Add figaro gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,10 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   
-  has_attached_file :avatar, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg"
+  has_attached_file :avatar, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg", 
+                    :storage => :dropbox,
+				            :dropbox_credentials => Rails.root.join("config/dropbox.yml")
+
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
   validates :name, presence: true
