6a6fc804a0d42d111d75856dfee2c7a6f4141e04
Tom Lynam <tom.s.lynam@gmail.com>
2014-10-04 18:38:18 -0400

    Introduce bet concept

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -12,6 +12,8 @@ class Listing < ActiveRecord::Base
 
 	belongs_to :user
 
+	has_many :bets
+
 	#has_attached_file :avatar, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg"
   	#validates_attachment_content_type :avatar, :content_type => %w(image/jpeg image/jpg image/png) 
 
