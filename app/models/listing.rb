556b4fbcd2509661e26a613b3b3a9a921074ae50
Tyler J. Diaz <me@tylerdiaz.com>
2015-03-11 21:31:26 -0400

    Clean up

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,20 +1,14 @@
 class Listing < ActiveRecord::Base
-
-	validates :name, :race, :cause, presence: true
-
-	validate :raceday_cannot_be_in_the_past
- 
-	def raceday_cannot_be_in_the_past
-	    if raceday.present? && raceday < Date.today
-	      errors.add(:raceday, "can't be in the past")
-	    end
-	end
-
 	belongs_to :user
-
 	has_many :bets
 
-	#has_attached_file :avatar, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg"
-  	#validates_attachment_content_type :avatar, :content_type => %w(image/jpeg image/jpg image/png) 
+	validates :name, :race, :cause, presence: true
+	validate :raceday_cannot_be_in_the_past
+
+	def raceday_cannot_be_in_the_past
+    if raceday.present? && raceday < Date.today
+      errors.add(:raceday, "can't be in the past")
+    end
+	end
 
 end
