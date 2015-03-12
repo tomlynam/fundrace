b94de0449c948fbf94184d5c54688710117b0d7a
Tom Lynam <tom.s.lynam@gmail.com>
2015-03-11 22:17:07 -0400

    rails upgrade schema

--- db/schema.rb
+++ db/schema.rb
@@ -13,46 +13,46 @@
 
 ActiveRecord::Schema.define(version: 20150312010319) do
 
-  create_table "bets", force: true do |t|
+  create_table "bets", force: :cascade do |t|
     t.integer  "listing_id"
-    t.string   "name"
-    t.string   "email"
+    t.string   "name",                  limit: 255
+    t.string   "email",                 limit: 255
     t.float    "starting_bet"
     t.float    "maximum_bet"
     t.float    "bonus_per_minute"
-    t.string   "projected_finish_time"
+    t.string   "projected_finish_time", limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   add_index "bets", ["listing_id"], name: "index_bets_on_listing_id"
 
-  create_table "listings", force: true do |t|
-    t.string   "name"
-    t.string   "race"
+  create_table "listings", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.string   "race",       limit: 255
     t.date     "raceday"
-    t.string   "cause"
+    t.string   "cause",      limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
   end
 
-  create_table "users", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  limit: 255, default: "", null: false
+    t.string   "encrypted_password",     limit: 255, default: "", null: false
+    t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",                      default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
+    t.string   "current_sign_in_ip",     limit: 255
+    t.string   "last_sign_in_ip",        limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "name"
-    t.string   "avatar_file_name"
-    t.string   "avatar_content_type"
+    t.string   "name",                   limit: 255
+    t.string   "avatar_file_name",       limit: 255
+    t.string   "avatar_content_type",    limit: 255
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
     t.string   "stripe_customer_id"
